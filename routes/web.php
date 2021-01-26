b2a4f623f5d9208fe2d74409169f027612407b66
Bartosz Kamzelski <bartek@bk.waw.pl>
2021-01-26 09:00:49 +0100

    JetStream Add

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,10 @@ Route::get('/', function () {
 });
 
 Route::resource('renters', RenterController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
